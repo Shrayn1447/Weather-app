e0c764dcae74f395500a4c6d02efb65b194a43f0
Eliseenko Alex <scanel2004@gmail.com>
2024-03-20 21:48:30 +0700

    fixed_url_weatherapi

--- src/app/components/CardWeather.jsx
+++ src/app/components/CardWeather.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-
+import Image from "next/image";
 export default function CardWeather({src,data,metric,children}) {
   return (
     <div className="w-[100px] h-[100px] flex flex-col items-center justify-center">
-      <img src={src} alt="#" height={60} width={60}/>
+      <Image src={src} alt={metric} height={60}  width={60}/>
       <h1 className=" font-bold text-blue-400">
         {children}
       </h1>
-      <h1>{data} {metric}</h1>
+      <div className="flex">
+        <h1>{data}</h1>
+        <h1>{metric}</h1>
+      </div>
     </div>
   );
 }
